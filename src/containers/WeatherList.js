f3486ee104046ac0df5e6d17f18bb5b40fa1e308
cgpomeroy <pomeroycolton@gmail.com>
2018-04-06 00:37:30 -0700

    Charts show average temperature, pressure and humidity for a five-day period.

---
+++ src/containers/WeatherList.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/Chart';
+
+class WeatherList extends Component {
+
+    renderWeather(cityData){
+        const temps = _.map(cityData.list.map(weather => weather.main.temp), temp=> (1.8*(temp-273))+32);
+        const pressure = cityData.list.map(weather => weather.main.pressure);
+        const humidity = cityData.list.map(weather => weather.main.humidity);
+
+
+        return(
+          <tr key={cityData.city.id}>
+              <td>{cityData.city.name}</td>
+              <td>
+                <Chart
+                    color="red"
+                    data={temps}
+                    units=" (°F)"
+                />
+              </td>
+              <td>
+                  <Chart
+                      color="grey"
+                      data={pressure}
+                      units=" (hPA)"
+                  />
+              </td>
+              <td>
+                  <Chart
+                      color="blue"
+                      data={humidity}
+                      units="%"
+                  />
+              </td>
+          </tr>
+        );
+    }
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+function mapStateToProps({ weather }) {
+    return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);

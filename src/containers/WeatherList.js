8a6b923109327236960c08750c68ff7fe95a99b5
cgpomeroy <pomeroycolton@gmail.com>
2018-04-06 21:18:43 -0700

    Added GoogleMaps component to replace city name.

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/Chart';
+import GoogleMap from "../components/GoogleMap";
+
 
 class WeatherList extends Component {
 
@@ -8,23 +10,25 @@ class WeatherList extends Component {
         const temps = _.map(cityData.list.map(weather => weather.main.temp), temp=> (1.8*(temp-273))+32);
         const pressure = cityData.list.map(weather => weather.main.pressure);
         const humidity = cityData.list.map(weather => weather.main.humidity);
+        const {id, name} = cityData.city;
+        const { lon, lat } = cityData.city.coord;
 
 
         return(
-          <tr key={cityData.city.id}>
-              <td>{cityData.city.name}</td>
+          <tr key={id}>
+              <td><GoogleMap lon={lon} lat={lat} /></td>
               <td>
                 <Chart
                     color="red"
                     data={temps}
-                    units=" (°F)"
+                    units=" °F"
                 />
               </td>
               <td>
                   <Chart
                       color="grey"
                       data={pressure}
-                      units=" (hPA)"
+                      units=" hPA"
                   />
               </td>
               <td>

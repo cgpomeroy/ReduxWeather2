4f671083593fb3fac4155aa0e0f5dfb91bf06739
Colt <31629714+cgpomeroy@users.noreply.github.com>
2018-04-08 23:34:56 -0700

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,8 +12,6 @@ export function fetchWeather(city){
 
     const request = axios.get(url);
 
-    console.log('Request: ', request);
-
     return {
         type: FETCH_WEATHER,
         payload: request
